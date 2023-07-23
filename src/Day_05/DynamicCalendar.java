65d694fa7c354605e9e4b0121a776c945504cb50
Kalidas Ghule <kalidasghule143@gmail.com>
2023-07-23 19:55:26 +0530

    day 5

--- src/Day_05/DynamicCalendar.java
+++ src/Day_05/DynamicCalendar.java
@@ -16,6 +16,7 @@ public class DynamicCalendar
         System.out.println("\nEnter a month : ");
         int month = sc.nextInt();
 
+        //binding the days with keys.
         HashMap<Integer, String> map = new HashMap<>();
         map.put(0,"SUN");
         map.put(1,"MON");
@@ -25,25 +26,34 @@ public class DynamicCalendar
         map.put(5,"FRI");
         map.put(6,"SAT");
 
+        //getting 1st day of the month
         LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
         System.out.println(firstDayOfMonth);
 
-        int firstDay = firstDayOfMonth.getDayOfWeek().getValue();
+        //getting year and month
         YearMonth yearMonthObject = YearMonth.of(year, month);
 
+        //Counting days in the month
         int daysInMonth = yearMonthObject.lengthOfMonth();
         System.out.println("Days in "+month+" month : "+daysInMonth);
 
+        //getting input from user for month
         System.out.println("\nEnter a week start day (0:SUN, 1:MON, 2:TUE, 3:WED, 4:THUR, 5:FRI, 6:SAT) : ");
         int weekStartDay = sc.nextInt();
         System.out.println();
 
+        //printing days got from user
         for (int i = weekStartDay ; i <weekStartDay+7 ; i++)
             System.out.print(map.get(i%7)+" ");
         System.out.println();
 
         int count=1;
         int temp = weekStartDay;
+
+        //getting 1st day of the month
+        int firstDay = firstDayOfMonth.getDayOfWeek().getValue();
+
+        //printing spaces before first day of month
         while (temp!=firstDay)
         {
             System.out.print("    ");
@@ -52,6 +62,7 @@ public class DynamicCalendar
             count++;
         }
 
+        //printing dates
         for (int i = 1; i <= daysInMonth; i++)
         {
             if(i<=9)
